f6cf77744f42fdd4233b4176a4388081ee92f7eb
amans96 <aman1996sisay@gmail.com>
2026-08-07 07:41:35 +0300

    now some files are fixed

--- Client/terteb/src/main.jsx
+++ Client/terteb/src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
-import router from "./routes/Router.jsx";
+import router from "./routes/router.jsx";
 import { AuthProvider } from "./context/AuthContext.jsx";
 
 import "./index.css";
